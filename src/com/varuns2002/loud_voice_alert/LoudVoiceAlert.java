d356dc15a748bd33e57af61618c305a4e1113dc3
Varun Shanbhag <30069380+VarunS2002@users.noreply.github.com>
2024-09-22 21:09:47 +0530

    Switched to creating the beep sound instead of using awt to fix no sound on some systems

--- src/com/varuns2002/loud_voice_alert/LoudVoiceAlert.java
+++ src/com/varuns2002/loud_voice_alert/LoudVoiceAlert.java
@@ -4,13 +4,13 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
-import java.awt.Toolkit;
 import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.Properties;
 import java.util.Scanner;
+import javax.sound.sampled.SourceDataLine;
 
 public class LoudVoiceAlert {
 
@@ -19,6 +19,41 @@ public class LoudVoiceAlert {
      * Adjust the value for your own microphone and preferred volume.
      */
     public static int THRESHOLD = 500;
+
+    public static class Beeper {
+
+        public static float SAMPLE_RATE = 8000f;
+
+        public static void tone(int hz, int msecs, double vol)
+                throws LineUnavailableException
+        {
+            byte[] buf = new byte[1];
+            AudioFormat af =
+                    new AudioFormat(
+                            SAMPLE_RATE, // sampleRate
+                            8,           // sampleSizeInBits
+                            1,           // channels
+                            true,        // signed
+                            false);      // bigEndian
+            SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
+            sdl.open(af);
+            sdl.start();
+            for (int i=0; i < msecs*8; i++) {
+                double angle = i / (SAMPLE_RATE / hz) * 2.0 * Math.PI;
+                buf[0] = (byte)(Math.sin(angle) * 127.0 * vol);
+                sdl.write(buf,0,1);
+            }
+            sdl.drain();
+            sdl.stop();
+            sdl.close();
+        }
+
+        static void beep() {
+            try {
+                Beeper.tone(400, 500, 1);
+            } catch (Exception ignored){}
+        }
+    }
 
     static class Recorder implements Runnable {
 
@@ -91,7 +126,8 @@ public class LoudVoiceAlert {
 
                 int currentVolume = (int) (Math.abs(rms) * 1000);
                 if (this.started && currentVolume > THRESHOLD) {
-                    Toolkit.getDefaultToolkit().beep();
+//                    Toolkit.getDefaultToolkit().beep();
+                    Beeper.beep();
                     System.out.println(currentVolume + ", " + new SimpleDateFormat("hh:mm:ss aa").format(new Date()));
                 }
                 // System.out.println("Amplitude: " + Math.abs(rms) * 1000 + ", Peak: " + Math.abs(peak));
